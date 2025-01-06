7fb7589659afd68aa245cd6d7a33c3b347ff7629
tejas101k <tejassingh649@rediffmail.com>
2025-01-06 12:23:55 +0000

    Revert "extras: Drop SystemUI-NumOverlap Clockstyle"
    This reverts commit 26ec4bc69b4774d46a30becafddd27058735eca4.

--- prebuilts.mk
+++ prebuilts.mk
@@ -38,6 +38,7 @@ PRODUCT_PACKAGES += \
     SystemUIClocks-Growth \
     SystemUIClocks-Inflate \
     SystemUIClocks-Metro \
+    SystemUIClocks-NumOverlap \
     SystemUIClocks-Weather
 
 # JudgeInterface
