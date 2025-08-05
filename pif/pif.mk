d05fe3698bfda5c2616857fa8d45543afd6c0e5e
Subratam6296 <padraramesh420@gmail.com>
2025-08-05 20:48:50 +0530

    pif: Change props
    Signed-off-by: Subratam6296 <padraramesh420@gmail.com>

--- pif/pif.mk
+++ pif/pif.mk
@@ -1,18 +1,15 @@
+# PIF values
 PRODUCT_PRODUCT_PROPERTIES += \
-    persist.sys.pihooks_mainline_BRAND?=google \
-    persist.sys.pihooks_mainline_DEVICE?=caiman \
-    persist.sys.pihooks_mainline_MANUFACTURER?=Google \
-    persist.sys.pihooks_mainline_PRODUCT?=caiman \
-    persist.sys.pihooks_BRAND?=google \
     persist.sys.pihooks_MANUFACTURER?=Google \
-    persist.sys.pihooks_DEVICE?=caiman \
+    persist.sys.pihooks_BRAND?=google \
     persist.sys.pihooks_PRODUCT?=caiman_beta \
-    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=32 \
+    persist.sys.pihooks_DEVICE?=caiman \
+    persist.sys.pihooks_ID?=BP31.250610.004 \
+    persist.sys.pihooks_RELEASE?=12 \
     persist.sys.pihooks_SECURITY_PATCH?=2025-07-05 \
-    persist.sys.pihooks_ID?=BP31.250610.004
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SDK_INT?=32
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PihooksGmsFp="google/caiman_beta/caiman:16/BP31.250610.004/13769805:user/release-keys" \
-    PihooksGmsModel="Pixel 9 Pro" \
-    PihooksBuildFp="google/caiman/caiman:14/AD1A.240530.047.U1/12150698:user/release-keys" \
-    PihooksBuildModel="Pixel 9 Pro"
+    PihooksGmsModel="Pixel 9 Pro"
